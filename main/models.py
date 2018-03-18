a10590e6638cfc6560ef18ffeefdc9a63955b322
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-18 14:57:22 +0530

    Login Done, Models Made, gitignore updated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	date = models.DateField(default = timezone.now())
+	owner = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Traded(models.Model):
+	product = models.ForeignKey(Product, on_delete = models.CASCADE)
+	receiver = models.ForeignKey(User, on_delete = models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import *
 
 def index(request):
-	return render(request, 'index.html', {})
+
+	errors = list()
+	if request.method == "POST":
+		loginForm = LoginForm(request.POST)
+		if loginForm.is_valid():
+			user = loginForm.cleaned_data['user']
+			password = loginForm.cleaned_data['password']
+			user = authenticate(username = user, password = password)
+
+			if user is not None:
+				login(request, user)
+				try:
+					return HttpResponseRedirect(request.GET['next'])
+				except:
+					return HttpResponseRedirect('/')
+			else:
+				errors.append("<span class='red-text'>Invalid Username or password</span>")
+		else:
+			errors.append(loginForm.errors)
+	else:
+		loginForm = LoginForm()
+	context = {
+		'errors' : errors,
+		'LoginForm' : loginForm 
+	}
+	return render(request, 'index.html', context) 
