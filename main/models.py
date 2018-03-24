97a03f1f9defd01b0b3e5e59e097d854573b21df
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-24 15:24:39 +0530

    category added, image bug fixes

--- main/models.py
+++ main/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+category_choice = [
+	"Electronics",
+	"HouseHold",
+	"Daily",
+	"Clothing",
+]
+
 class Product(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	image = models.ImageField(upload_to = 'main/static/pic_folder/', blank = False, null = True)
+	category = models.IntegerField(choices = tuple([ x for x in enumerate(category_choice)]))
 	date = models.DateField(default = timezone.now())
 	owner = models.ForeignKey(User, on_delete = models.CASCADE)
 
