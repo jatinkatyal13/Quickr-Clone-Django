ed0a290858826a4d7dd820d1f7ce15cc5bb939b2
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-26 00:05:23 +0530

    Image Bug Fixed

--- main/models.py
+++ main/models.py
@@ -14,10 +14,13 @@ category_choice = [
 class Product(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
-	image = models.ImageField(upload_to = 'pic_folder/', blank = False, null = True)
+	image = models.ImageField(upload_to = 'main/static/pic_folder/', blank = False, null = True)
 	category = models.IntegerField(choices = tuple([ x for x in enumerate(category_choice)]))
 	date = models.DateField(default = timezone.now())
 	owner = models.ForeignKey(User, on_delete = models.CASCADE)
+
+	def getImagePath(self):
+		return "/".join(self.image.name.split('/')[2:])
 
 class TradeRequest(models.Model):
 	product = models.ForeignKey(Product, on_delete = models.CASCADE, related_name="trade_request_product")
