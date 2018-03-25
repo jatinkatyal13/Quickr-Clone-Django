d75a28773033213fb6d6a2bac778ca59763dca5c
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-25 23:10:37 +0530

    Google Login Added

--- django_quickr/settings.py
+++ django_quickr/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -65,10 +66,21 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+		'social_django.context_processors.backends', 
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+	'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+	'social_core.backends.google.GoogleOpenId',  # for Google authentication
+	'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+	'social_core.backends.github.GithubOAuth2',  # for Github authentication
+	'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+	'django.contrib.auth.backends.ModelBackend',
+)
 
 WSGI_APPLICATION = 'django_quickr.wsgi.application'
 
@@ -124,3 +136,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/form/login'
 
+LOGIN_REDIRECT_URL = '/'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='946965200127-6dm6rjfa6m8ro8hnbvcepa93avb8rtn4.apps.googleusercontent.com'  #Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'OIhrzQWYYUHgGwzEcMzTemOz' #Paste Secret Key
--- django_quickr/urls.py
+++ django_quickr/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^auth/', include('social_django.urls', namespace='social')),
     url(r'^/?', include('main.urls')),
 ]
