b81a64a4f35b33729065117d486742b3b4902b7e
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-29 00:31:17 +0530

    typo

--- django_quickr/settings.py
+++ django_quickr/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'django_quickr.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'),
+    'default': dj_database_url.config(default=config('DATABASE_URL')),
     'development': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
