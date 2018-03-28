b35761eaca62438ac8232827a4e33d19f3d8dfda
Jatin Katyal <jatin.katyal13@gmail.com>
2018-03-29 00:34:43 +0530

    static issue resolved

--- django_quickr/settings.py
+++ django_quickr/settings.py
@@ -132,8 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+	os.path.join(PROJECT_ROOT, 'main/static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_URL = '/form/login'
 
